0154c0139c4d7d768fbe8ed217d2b5eb4ff1f1ee
melvinkukwansafari <melvin.kukwan@student.moringaschool.com>
2023-01-21 07:37:37 +0300

    partially completed

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,27 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
+import Links from './Links';
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
 
-function App() {
+function App(props) {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home user={props.name}/>
+      <About/>
+    <Links 
+      github= "https://github.com/liza"
+      linkedin="https://www.linkedin.com/in/liza/"
+    />
     </div>
   );
 }
+
+
+
+
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
-
+import user from "../data/user.js"
 function Home(props) {
+  
   return (
     <div id="home">
-      <h1 style={{ color: props.color }}>
-        {props.name} is a Web Developer from {props.city}
+      <h1 style={{ color: "blue" }} >
+      {props.name} is a Web Developer from {props.city}
       </h1>
     </div>
   );
